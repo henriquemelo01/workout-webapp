fe45555d93ef773bafbd966fffd4e89b974a326f
Henrique Moraes <henriquemsm@outlook.com>
2021-01-24 14:22:21 -0300

    Refatorando o código - Paradigma OOP

--- script.js
+++ script.js
@@ -11,79 +11,99 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// Variáveis Globais
-let map, mapEvent;
+// Classes: App, Workout -> Running, Cycling
 
-// Leaflet - Map library:
-const dspMap = function (latitude, longitude) {
-  // Create a map in the 'map' div (l.map("div id"))
-  map = L.map('map').setView([latitude, longitude], 15);
+class App {
+  // Private Var
+  #map;
+  #mapEvent;
 
-  // Layout do mapa
-  L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-    maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-  }).addTo(map);
+  constructor() {
+    // Display Map, assim que criarmos uma instância.
+    this._getPosition();
 
-  // Quando clicar no mapa exibir um marker na posição em que clicamos
-  //  O metodo on da library funciona como o addEventListener
-  map.on('click', function (mapE) {
-    mapEvent = mapE; // Objeto que contém coordenas do click no map
-    const { lat, lng } = mapE.latlng;
+    // Events
+
+    // Disparar evento de submit toda vez que o usuário apertar enter em algum campo do form
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    // Evento mudança de tipo de exercício
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  // Metodos que serão herdados por meio do prototype (App.protoype) - Instance Methods
+
+  _getPosition() {
+    // Para evitar possíveis erros em Browsers antigos, só chamamos o metodo getCurrentPosition se a API estiver disponível
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () =>
+        alert('Não foi possível encontrar sua localização')
+      );
+  }
+
+  // Sucess Function - getCurrentPosition
+  _loadMap(geoLocObj) {
+    //   Destructuing Objects: Extrair dado de uma propriedade e armazenar em uma variável de mesmo nome
+    const { latitude, longitude } = geoLocObj.coords;
+    const position = [latitude, longitude];
+    // Create a map in the 'map' div (l.map("div id"))
+    this.#map = L.map('map').setView(position, 15);
+
+    // Layout do mapa
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    }).addTo(this.#map);
+
+    //  O metodo on da library funciona como o addEventListener
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    // Como queremos redefinir o valor da propriedade #mapEvent da instancia que foi criada pela class App, utiliza-se o metodo bind para setar o this, uma vez que esta apontando para o elemento o qual o evento esta ligado (this.#map)
+
+    this.#mapEvent = mapE; // Objeto que contém coordenas do click no map
 
     // Display form
     form.classList.remove('hidden');
     inputDistance.focus();
-  });
-};
+  }
 
-// Geolocation: É uma API que esta disponível nos Browsers:
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-// getCurrentPostion(sucess callback, error call back)
+  _newWorkout(e) {
+    // Por padrão ao apertar enter em um form a página é recarregada
+    e.preventDefault();
 
-// Para evitar possíveis erros em Browsers antigos, só chamamos o metodo getCurrentPosition se a API estiver disponível
-if (navigator.geolocation);
-navigator.geolocation.getCurrentPosition(
-  geoLocObj => {
-    //   Destructuing Objects: Extrair dado de uma propriedade e armazenar em uma variável de mesmo nome
-    const { latitude } = geoLocObj.coords;
-    const { longitude } = geoLocObj.coords;
-    dspMap(latitude, longitude);
-  },
-  () => alert('Não foi possível encontrar sua localização')
-);
+    // limpar form
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+      '';
 
-// Disparar evento de submit toda vez que o usuário apertar enter em algum campo do form
-form.addEventListener('submit', function (e) {
-  // Por padrão ao apertar enter em um form a página é recarregada
-  e.preventDefault();
+    // Display Marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          // Personalizar Popup
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false, // autoClose: fecha o popup toda vez que algum outro estiver aberto
+          closeOnClick: false,
+          className: 'running-popup', // CSS class
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
 
-  // limpar form
-  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
-    '';
+// Precisa ser criado após o mapa ser carregado
 
-  // Display Marker
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        // Personalizar Popup
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false, // autoClose: fecha o popup toda vez que algum outro estiver aberto
-        closeOnClick: false,
-        className: 'running-popup', // CSS class
-      })
-    )
-    .setPopupContent('Workout')
-    .openPopup();
-});
+const app = new App();
+console.log(app);
 
-// Evento mudança de tipo de exercício
-inputType.addEventListener('change', function () {
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-});
-
-// Architecture: Where the data wil be store
+// OBS: Chamando a successful callback no metodo getCurrentPosition() -> Como o this da callback não esta apontando para o objeto foi necessário usar o metodo bind (retorna uma nova função) para setarmos o this manualmente
