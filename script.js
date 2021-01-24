a885fb6a2dbb33389d25c452757c7f4beee13d0b
Henrique Moraes <henriquemsm@outlook.com>
2021-01-24 12:15:25 -0300

    Submit Event - Add marker when user submits the form

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// Variáveis Globais
+let map, mapEvent;
+
 // Leaflet - Map library:
 const dspMap = function (latitude, longitude) {
   // Create a map in the 'map' div (l.map("div id"))
-  const map = L.map('map').setView([latitude, longitude], 15);
+  map = L.map('map').setView([latitude, longitude], 15);
 
   // Layout do mapa
   L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
@@ -22,9 +25,16 @@ const dspMap = function (latitude, longitude) {
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
   }).addTo(map);
 
-  //   Marker
-  L.marker([latitude, longitude]).addTo(map).bindPopup('User Location');
-  // .openPopup();
+  // Quando clicar no mapa exibir um marker na posição em que clicamos
+  //  O metodo on da library funciona como o addEventListener
+  map.on('click', function (mapE) {
+    mapEvent = mapE; // Objeto que contém coordenas do click no map
+    const { lat, lng } = mapE.latlng;
+
+    // Display form
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  });
 };
 
 // Geolocation: É uma API que esta disponível nos Browsers:
@@ -42,3 +52,38 @@ navigator.geolocation.getCurrentPosition(
   },
   () => alert('Não foi possível encontrar sua localização')
 );
+
+// Disparar evento de submit toda vez que o usuário apertar enter em algum campo do form
+form.addEventListener('submit', function (e) {
+  // Por padrão ao apertar enter em um form a página é recarregada
+  e.preventDefault();
+
+  // limpar form
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+    '';
+
+  // Display Marker
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        // Personalizar Popup
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false, // autoClose: fecha o popup toda vez que algum outro estiver aberto
+        closeOnClick: false,
+        className: 'running-popup', // CSS class
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+// Evento mudança de tipo de exercício
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
+
+// Architecture: Where the data wil be store
